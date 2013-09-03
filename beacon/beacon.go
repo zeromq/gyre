073c5c21c24dc8a1ddd8b00b0b47e7a0249f2992
Armen Baghumian <armen@OpenSourceClub.org>
2013-09-03 20:37:59 +1000

    Very first implementation of beacon package
    Should be tested in multiple machines

---
+++ beacon/beacon.go
@@ -0,0 +1,175 @@
+// Package beacon implements a peer-to-peer discovery service for local
+// networks. A beacon can broadcast and/or capture service announcements
+// using UDP messages on the local area network. This implementation uses
+// IPv4 UDP broadcasts. You can define the format of your outgoing beacons,
+// and set a filter that validates incoming beacons. Beacons are sent and
+// received asynchronously in the background.
+//
+// This package is an idiomatic go translation of zbeacon class of czmq at
+// following address:
+//      https://github.com/zeromq/czmq
+//
+// Instead of ZMQ_PEER socket it uses go channel and also uses go routine
+// instead of zthread.
+//
+package beacon
+
+import (
+	"bytes"
+	"net"
+	"time"
+)
+
+const (
+	beaconMax       = 255
+	defaultInterval = 1 * time.Second
+)
+
+type Signal struct {
+	Addr     string
+	Transmit []byte
+}
+
+type beacon struct {
+	signals    chan *Signal
+	ticker     <-chan time.Time
+	conn       *net.UDPConn
+	interval   time.Duration
+	noecho     bool
+	terminated bool
+	transmit   []byte
+	filter     []byte
+	addr       string
+	port       int
+	broadcast  string // TODO: need to figure out the broadcast address
+	hostname   string
+}
+
+// New creates a new beacon
+func New(port int) (*beacon, error) {
+
+	bcast := &net.UDPAddr{Port: port, IP: net.IPv4bcast}
+	conn, err := net.ListenUDP("udp", bcast)
+	if err != nil {
+		return nil, err
+	}
+
+	var hostname string
+	addr := conn.LocalAddr().String()
+	name, err := net.LookupAddr(addr)
+	if err == nil {
+		hostname = name[0]
+	} else {
+		hostname = addr
+	}
+
+	b := &beacon{
+		signals:  make(chan *Signal),
+		conn:     conn,
+		interval: defaultInterval,
+		hostname: hostname,
+		addr:     addr,
+		port:     port,
+	}
+
+	go b.beaconReader()
+	go b.handle()
+
+	return b, nil
+}
+
+// Close terminates the beacon
+func (b *beacon) Close() {
+	b.terminated = true
+	close(b.signals)
+}
+
+// SetInterval sets broadcast interval
+func (b *beacon) SetInterval(interval time.Duration) *beacon {
+	b.interval = interval
+	return b
+}
+
+// NoEcho filters out any beacon that looks exactly like ours
+func (b *beacon) NoEcho() *beacon {
+	b.noecho = true
+	return b
+}
+
+// Publish starts broadcasting beacon to peers at the specified interval
+func (b *beacon) Publish(transmit []byte) *beacon {
+	b.transmit = transmit
+	if b.interval == 0 {
+		b.ticker = time.After(defaultInterval)
+	} else {
+		b.ticker = time.After(b.interval)
+	}
+	return b
+}
+
+// Silence stops broadcasting beacon
+func (b *beacon) Silence() *beacon {
+	b.transmit = nil
+	return b
+}
+
+// Subscribe starts listening to other peers; zero-sized filter means get everything
+func (b *beacon) Subscribe(filter []byte) *beacon {
+	b.filter = filter
+	return b
+}
+
+// Unsubscribe stops listening to other peers
+func (b *beacon) Unsubscribe() *beacon {
+	b.filter = nil
+	return b
+}
+
+// Hostname returns our own IP address as printable string
+func (b *beacon) Hostname() string {
+	return b.hostname
+}
+
+// Signals returns signals channel
+func (b *beacon) Signals() chan *Signal {
+	return b.signals
+}
+
+func (b *beacon) beaconReader() {
+	for {
+		buff := make([]byte, beaconMax)
+		n, addr, err := b.conn.ReadFrom(buff)
+		if err != nil || n > beaconMax {
+			continue
+		}
+
+		send := bytes.HasPrefix(buff[:n], b.filter)
+		if send && b.noecho {
+			send = !bytes.Equal(buff[:n], b.transmit)
+		}
+
+		if send {
+			b.signals <- &Signal{addr.String(), buff[:n]}
+		}
+	}
+}
+
+func (b *beacon) handle() {
+	for {
+		select {
+		case <-b.ticker:
+			if b.terminated {
+				break
+			}
+			b.send()
+			b.ticker = time.After(b.interval)
+		}
+	}
+}
+
+func (b *beacon) send() {
+	if b.transmit != nil {
+		bcast := &net.UDPAddr{Port: b.port, IP: net.IPv4bcast}
+		b.conn.WriteTo(b.transmit, bcast)
+	}
+}
---
+++ beacon/beacon_test.go
@@ -0,0 +1,83 @@
+package beacon
+
+import (
+	"bytes"
+	"testing"
+	"time"
+)
+
+func TestBeacon(t *testing.T) {
+	transmit := []byte("SAMPLE-BEACON")
+	b, _ := New(9999)
+	b.SetInterval(50 * time.Millisecond)
+	b.Publish(transmit)
+
+	select {
+	case <-time.After(1 * time.Second):
+		t.Fatalf("expected to receive a signal but got nothing!")
+	case signal := <-b.Signals():
+		if !bytes.Equal(transmit, signal.Transmit) {
+			t.Fatalf("expected % X, got % X", transmit, signal.Transmit)
+		}
+	}
+
+	b.Silence()
+
+	select {
+	case <-time.After(300 * time.Millisecond):
+	case signal := <-b.Signals():
+		t.Fatalf("expected silence, got %v", signal)
+	}
+
+	b.NoEcho()
+
+	select {
+	case <-time.After(300 * time.Millisecond):
+	case signal := <-b.Signals():
+		t.Fatalf("expected to not recive an echo, got %q, %q", signal.Addr, signal.Transmit)
+	}
+
+	b.Close()
+	select {
+	case <-time.After(1 * time.Second):
+		t.Fatalf("expected to receive a close signal but got nothing!")
+	case _, ok := <-b.Signals():
+		if ok {
+			t.Fatalf("Signals() is not closed!")
+		}
+	}
+
+	node1, _ := New(5670)
+	node2, _ := New(5670)
+	node3, _ := New(5670)
+
+	node1.SetInterval(50 * time.Millisecond)
+	node2.SetInterval(50 * time.Millisecond)
+	node3.SetInterval(50 * time.Millisecond)
+
+	node1.NoEcho()
+	node1.Subscribe([]byte("NODE"))
+	node1.Publish([]byte("NODE/1"))
+	node2.Publish([]byte("NODE/2"))
+	node3.Publish([]byte("GARBAGE"))
+	node2.Subscribe([]byte("NODE/1"))
+	node3.Subscribe([]byte("SOMETHING"))
+
+	for i := 0; i < 10; i++ {
+		select {
+		case signal := <-node1.Signals():
+			expected := []byte("NODE/2")
+			if !bytes.Equal(expected, signal.Transmit) {
+				t.Fatalf("expected %s, got %s", expected, signal.Transmit)
+			}
+		case signal := <-node2.Signals():
+			expected := []byte("NODE/1")
+			if !bytes.Equal(expected, signal.Transmit) {
+				t.Fatalf("expected %s, got %s", expected, signal.Transmit)
+			}
+		case signal := <-node3.Signals():
+			t.Fatalf("not expected to recieve any signal from node3, got %q, %q", signal.Addr, signal.Transmit)
+		}
+		time.Sleep(25 * time.Millisecond)
+	}
+}
