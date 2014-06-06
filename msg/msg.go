f949b1b6913b73aa861facd85d174fa90eec4719
Armen Baghumian <armen@OpenSourceClub.org>
2014-06-06 00:01:48 +0000

    Fixed formating

--- msg/msg.go
+++ msg/msg.go
@@ -1,10 +1,10 @@
 // Package Msg is 100% generated. If you edit this file,
 // you will lose your changes at the next build cycle.
-// DO NOT MAKE ANY CHANGES YOU WISH TO KEEP. The correct places
-// for commits are:
+// DO NOT MAKE ANY CHANGES YOU WISH TO KEEP.
 //
-// - The XML model used for this code generation: zre_msg.xml
-// - The code generation script that built this file: codec_go.gsl
+// The correct places for commits are:
+//  - The XML model used for this code generation: zre_msg.xml
+//  - The code generation script that built this file: codec_go.gsl
 package msg
 
 import (
