47d8120c28f0670cd0e767fae960c13412fa6eab
Armen Baghumian <armen@OpenSourceClub.org>
2014-06-13 11:40:00 +0000

    Added doc file

---
+++ examples/ping/doc.go
@@ -0,0 +1,18 @@
+/*
+Ping other peers in a ZRE network
+
+Usage:
+
+    ping [Options]
+
+Examples:
+
+    ping -group GRP
+
+Options:
+
+Usage of ping:
+
+  -group="GLOBAL": The group we are going to join
+*/
+package main
