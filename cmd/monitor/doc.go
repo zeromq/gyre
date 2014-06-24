c4204ebc123de7bb764946cace991554f20e54c8
Armen Baghumian <armen@OpenSourceClub.org>
2014-06-24 23:39:37 +0000

    Introduced monitor tool to dump useful information about ZRE net traffic

---
+++ cmd/monitor/doc.go
@@ -0,0 +1,19 @@
+/*
+Monitors ZRE network traffic
+
+Usage:
+
+    monitor [Options]
+
+Examples:
+
+    monitor -group GROUP_NAME
+
+Options:
+
+Usage of monitor:
+
+  -group="GLOBAL": The group we are going to join
+  -verbose=true: Set verbose flag
+*/
+package main
--- cmd/monitor/monitor.go
+++ cmd/monitor/monitor.go
@@ -10,8 +10,9 @@ import (
 )
 
 var (
-	input = make(chan string)
-	group = flag.String("group", "GLOBAL", "The group we are going to join")
+	input   = make(chan string)
+	group   = flag.String("group", "GLOBAL", "The group we are going to join")
+	verbose = flag.Bool("verbose", true, "Set verbose flag")
 )
 
 func ping() {
@@ -24,6 +25,9 @@ func ping() {
 	}
 	defer node.Stop()
 
+	if *verbose {
+		node.SetVerbose()
+	}
 	err = node.Start()
 	if err != nil {
 		log.Fatalln(err)
@@ -36,12 +40,19 @@ func ping() {
 			switch e.Type() {
 			case gyre.EventEnter:
 				log.Printf("[%s] peer %q entered\n", node.Name(), e.Name())
-				node.Whisper(e.Sender(), []byte("Hello"))
+
 			case gyre.EventExit:
 				log.Printf("[%s] peer %q exited\n", node.Name(), e.Name())
+
+			case gyre.EventJoin:
+				log.Printf("[%s] peer %q joined to %s\n", node.Name(), e.Name(), e.Group())
+
+			case gyre.EventLeave:
+				log.Printf("[%s] peer %q left\n", node.Name(), e.Name())
+
 			case gyre.EventWhisper:
 				log.Printf("[%s] received ping (WHISPER) from %q\n", node.Name(), e.Name())
-				node.Shout(*group, []byte("Hello"))
+
 			case gyre.EventShout:
 				log.Printf("[%s] (%s) received a ping (SHOUT) from %q\n", node.Name(), e.Group(), e.Name())
 			}
