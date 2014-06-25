b40c3c12ba1bbc3277a1927555e801e65a290185
Armen Baghumian <armen@OpenSourceClub.org>
2014-06-25 10:51:08 +0000

    Monitor cmd monitors ZRE traffi

--- cmd/monitor/doc.go
+++ cmd/monitor/doc.go
@@ -13,7 +13,7 @@ Options:
 
 Usage of monitor:
 
-  -group="GLOBAL": The group we are going to join
+  -group="*": The group we are going to join. By default joins every group in the network. For multiple groups separate groups with comma.
   -verbose=true: Set verbose flag
 */
 package main
--- cmd/monitor/monitor.go
+++ cmd/monitor/monitor.go
@@ -7,11 +7,12 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"strings"
 )
 
 var (
 	input   = make(chan string)
-	group   = flag.String("group", "GLOBAL", "The group we are going to join")
+	group   = flag.String("group", "*", "The group we are going to join. By default joins every group in the network. For multiple groups separate groups with comma.")
 	verbose = flag.Bool("verbose", true, "Set verbose flag")
 )
 
@@ -28,11 +29,17 @@ func ping() {
 	if *verbose {
 		node.SetVerbose()
 	}
+
 	err = node.Start()
 	if err != nil {
 		log.Fatalln(err)
 	}
-	node.Join(*group)
+
+	if *group != "*" {
+		for _, g := range strings.Split(*group, ",") {
+			node.Join(strings.TrimSpace(g))
+		}
+	}
 
 	for {
 		select {
@@ -46,15 +53,18 @@ func ping() {
 
 			case gyre.EventJoin:
 				log.Printf("[%s] peer %q joined to %s\n", node.Name(), e.Name(), e.Group())
+				if *group == "*" {
+					node.Join(e.Group())
+				}
 
 			case gyre.EventLeave:
 				log.Printf("[%s] peer %q left\n", node.Name(), e.Name())
 
 			case gyre.EventWhisper:
-				log.Printf("[%s] received ping (WHISPER) from %q\n", node.Name(), e.Name())
+				log.Printf("[%s] received a WHISPER from %q\n", node.Name(), e.Name())
 
 			case gyre.EventShout:
-				log.Printf("[%s] (%s) received a ping (SHOUT) from %q\n", node.Name(), e.Group(), e.Name())
+				log.Printf("[%s] received a SHOUT targeted to %s group from %q\n", node.Name(), e.Group(), e.Name())
 			}
 		case <-c:
 			return
