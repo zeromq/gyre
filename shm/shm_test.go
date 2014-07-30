96ccfc58804b9ae6d4d388b0add5c4f53eec2daf
Armen Baghumian <armen@OpenSourceClub.org>
2014-07-30 22:11:42 +0000

    Now imports are organized, std lib packages are in the first group

--- shm/shm_test.go
+++ shm/shm_test.go
@@ -2,6 +2,7 @@ package shm
 
 import (
 	"bytes"
+	"sync"
 	"testing"
 )
 
@@ -11,7 +12,7 @@ func TestShm(t *testing.T) {
 
 	hm := New()
 	hm.Subtree("This/Is/It").Node("1").SetVal(testVal)
-	val := hm.Subtree("This/Is/It").Node("1").Val
+	val := hm.Subtree("This/Is/It").Node("1").Val()
 
 	if !bytes.Equal(val, testVal) {
 		t.Errorf("expected [% x] but got [% x]", testVal, val)
@@ -38,20 +39,64 @@ func TestShm(t *testing.T) {
 	}
 
 	hm.Subtree("This/Is/It").DelNode("1")
-	emptyVal := hm.Subtree("This/Is/It").Node("1").Val
+	emptyVal := hm.Subtree("This/Is/It").Node("1").Val()
 	if !bytes.Equal(emptyVal, []byte("")) {
 		t.Errorf("expected an empty val but got [% x]", emptyVal)
 	}
 
 	hm.Subtree("This/Is/It2").Node("2").SetVal(testVal2)
-	val = hm.Subtree("This/Is/It2").Node("2").Val
+	val = hm.Subtree("This/Is/It2").Node("2").Val()
 	if !bytes.Equal(val, testVal2) {
 		t.Errorf("expected [% x] but got [% x]", testVal, val)
 	}
 
 	hm.DelSubtree("This/Is/It2")
-	emptyVal = hm.Subtree("This/Is/It2").Node("2").Val
+	emptyVal = hm.Subtree("This/Is/It2").Node("2").Val()
 	if !bytes.Equal(emptyVal, []byte("")) {
 		t.Errorf("expected an empty val but got [% x]", emptyVal)
 	}
+}
+
+// Just to test with -race parameter: go test -race
+func TestShmParallel(t *testing.T) {
+	testVal := []byte("Sample Value")
+	testVal2 := []byte("Sample Value2")
+
+	hm := New()
+
+	var wg sync.WaitGroup
+	wg.Add(1000)
+
+	bcast := make(chan struct{})
+
+	for i := 0; i < 1000; i++ {
+
+		go func() {
+			defer wg.Done()
+
+			select {
+			case <-bcast:
+			}
+
+			hm.Subtree("This/Is/It").Node("1").SetVal(testVal)
+			val := hm.Subtree("This/Is/It").Node("1").Val()
+			hm.Subtree("This/Is/It").Node("1").Props["foo"] = "bar"
+			hm.Subtree("This/Is/It").Node("1").Props.Get("foo")
+			hm.Subtree("This/Is/It").Node("1").Props.Get("foo2")
+			hm.Subtree("This/Is/It").Node("1").Props.GetOk("foo3")
+			hm.Subtree("This/Is/It").DelNode("1")
+			hm.Subtree("This/Is/It").Node("1").SetVal(val)
+			val = hm.Subtree("This/Is/It").Node("1").Val()
+			hm.Subtree("This/Is/It2").Node("2").SetVal(testVal2)
+			val = hm.Subtree("This/Is/It2").Node("2").Val()
+			hm.DelSubtree("This/Is/It2")
+			val = hm.Subtree("This/Is/It2").Node("2").Val()
+		}()
+	}
+
+	// Start all the go routins by broadcasting
+	close(bcast)
+
+	// Wait for them
+	wg.Wait()
 }
